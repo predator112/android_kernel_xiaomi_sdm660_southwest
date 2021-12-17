8bcf557b2ffc19a1e1ed49fd2ce88fdab582f7d7
iqbal <mi460334@gmail.com>
2021-12-17 14:25:51 +0000

    p

--- drivers/power/supply/qcom/qpnp-smb2.c
+++ drivers/power/supply/qcom/qpnp-smb2.c
@@ -2178,7 +2178,7 @@ static int smb2_init_hw(struct smb2 *chip)
 		break;
 	}
 #ifdef CONFIG_MACH_LONGCHEER
-	rc = vote(chg->chg_disable_votable, DEFAULT_VOTER, false, 0)	//add for 99% don't charge when power off in 2018.03.01
+	rc = vote(chg->chg_disable_votable, DEFAULT_VOTER, false, 0);	//add for 99% don't charge when power off in 2018.03.01
 #endif
 	if (rc < 0) {
 		dev_err(chg->dev, "Couldn't configure charge inhibit threshold rc=%d\n",
