8cd7419be7a7ffa4a15b4c4489469ff387bdb66d
iqbal <mi460334@gmail.com>
2022-01-29 12:13:06 +0000

    Revert "(halium) binder: do not use a separate queue for async oneway transactions"
    This reverts commit ff8d579a773c6596dcbcaa2cdaf2f39ad318e5a8.

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -2904,10 +2904,7 @@ static bool binder_proc_transaction(struct binder_transaction *t,
 	if (oneway) {
 		BUG_ON(thread);
 		if (node->has_async_transaction) {
-			// Halium: possible libgbinder bug workaround
-			/*pending_async = true;*/
-			pr_info("%d has pending async transaction, but still adding a new transaction to todo list (gbinder bug workaround)\n",
-					proc->pid);
+			pending_async = true;
 		} else {
 			node->has_async_transaction = true;
 		}
@@ -3903,8 +3900,7 @@ static int binder_thread_write(struct binder_proc *proc,
 
 				buf_node = buffer->target_node;
 				binder_node_inner_lock(buf_node);
-				// Halium: possible libgbinder bug workaround
-				/*BUG_ON(!buf_node->has_async_transaction);*/
+				BUG_ON(!buf_node->has_async_transaction);
 				BUG_ON(buf_node->proc != proc);
 				w = binder_dequeue_work_head_ilocked(
 						&buf_node->async_todo);
